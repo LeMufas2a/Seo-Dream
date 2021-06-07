4621ec8eabe15decfd640c142f837589b5238359
LeMufas2a <codage.mufasa@gmail.com>
2021-06-07 16:05:20 +0200

    template integrer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Seo Dream by Mufasa</title>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{asset('css/templatemo-seo-dream.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animated.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.css')}}">
+</head>
+
+<body>
+    @yield('content')
+
+    <!-- Scripts -->
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('js/owl-carousel.js')}}"></script>
+    <script src="{{asset('js/animation.js')}}"></script>
+    <script src="{{asset('js/imagesloaded.js')}}"></script>
+    <script src="{{asset('js/custom.js')}}"></script>
+</body>
+
+</html>
